bfd5fa4b9d020a976ff5d4168085f566a9343e3d
James <james.a.forbes@gmail.com>
2015-02-14 18:45:07 +1100

    Draw image

--- app.js
+++ app.js
@@ -1,63 +1,3 @@
-systems = {
-	Screen: function(){
-		_.each(C('Screen'),function(screen,id){
-			screen.el.width = window.innerWidth * screen.width
-			screen.el.height = window.innerHeight * screen.height
-
-			screen.con.translate(screen.translate[0] * window.innerWidth,screen.translate[0] *window.innerHeight)
-		})
-	},
-
-	KeyboardActivated: function(){
-		_.each(C('KeyboardActivated'),function(kb,id){
-				_.each(kb,function(componentsToActivate, keyName){
-					if(Keys[keyName]){
-						_.each(componentsToActivate,function(component,componentName){
-							C(componentName,component,id)
-						})
-					}
-				})
-		})
-	},
-
-	AddVelocity: function(){
-		_.each(C('AddVelocity'),function(add,id){
-			var v = C('Velocity',id)
-			v.x += add.x || 0
-			v.y += add.y || 0
-		})
-	},
-
-	Move: function(){
-		_.each(C('Velocity'),function(v,id){
-			var p = C('Location',id);
-			p.x += v.x || 0
-			p.y += v.y || 0
-		})
-	},
-
-	Draw: function(){
-		var canvas = C('Screen',1).el
-		var con = canvas.getContext('2d')
-
-		_.each(C('Drawable'),function(drawable,id){
-			var p = C('Location',id)
-			var d = C('Dimensions',id)
-
-			//console.log(p,d)
-			con.fillRect(
-				p.x - d.width/2,
-				p.y - d.height/2,
-				d.width, d.height
-			)
-		})
-	},
-
-	CleanUp: function(){
-		delete C.components.AddVelocity
-	}
-}
-
 C({
 	Screen: { width: 1, height: 1, el: document.querySelector('canvas'), con: document.querySelector('canvas').getContext('2d'), translate: [0.5, 0.5] }
 })
@@ -67,6 +7,7 @@ player = C({
 	Location: {x:0,y:0},
 	Velocity: {x:0,y:0},
 	Dimensions: { width: 20, height: 20},
+	Sprite: { image: s_player },
 	KeyboardActivated: {
 		'LEFT': {
 			AddVelocity: { x: -0.1 }
---
+++ systems.js
@@ -0,0 +1,65 @@
+systems = {
+	Screen: function(){
+		_.each(C('Screen'),function(screen,id){
+			screen.el.width = window.innerWidth * screen.width
+			screen.el.height = window.innerHeight * screen.height
+
+			screen.con.translate(screen.translate[0] * window.innerWidth,screen.translate[0] *window.innerHeight)
+
+			if(!screen.smoothing){
+				;['','moz','ms','webkit'].map(function(prefix){
+					screen.con[prefix+"ImageSmoothingEnabled"] = false
+				})
+			}
+		})
+	},
+
+	KeyboardActivated: function(){
+		_.each(C('KeyboardActivated'),function(kb,id){
+			_.each(kb,function(componentsToActivate, keyName){
+				if(Keys[keyName]){
+					_.each(componentsToActivate,function(component,componentName){
+						C(componentName,component,id)
+					})
+				}
+			})
+		})
+	},
+
+	AddVelocity: function(){
+		_.each(C('AddVelocity'),function(add,id){
+			var v = C('Velocity',id)
+			v.x += add.x || 0
+			v.y += add.y || 0
+		})
+	},
+
+	Move: function(){
+		_.each(C('Velocity'),function(v,id){
+			var p = C('Location',id);
+			p.x += v.x || 0
+			p.y += v.y || 0
+		})
+	},
+
+	Draw: function(){
+		var canvas = C('Screen',1).el
+		var con = canvas.getContext('2d')
+
+		_.each(C('Sprite'),function(sprite,id){
+			var p = C('Location',id)
+			var d = C('Dimensions',id)
+
+			con.drawImage(
+				sprite.image,
+				p.x - d.width/2,
+				p.y - d.height/2,
+				d.width, d.height
+			)
+		})
+	},
+
+	CleanUp: function(){
+		delete C.components.AddVelocity
+	}
+}
